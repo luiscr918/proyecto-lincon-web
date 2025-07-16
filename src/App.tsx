36dd10400660e02340c2319715162e31f2081bf6
DanielMP1995 <danielsantiagomeraparedes@gmail.com>
2025-07-16 00:13:18 -0500

    Nosotros-Encabezado

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
+import { Nosotros } from "./pages/Nosotros";
+
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/nosotros" element={<Nosotros />} />
     </Routes>
   );
 }
---
+++ src/pages/Nosotros.tsx
@@ -0,0 +1,14 @@
+import '../components/QuienesSomos.css'
+export const Nosotros = () => {
+  return (
+    <div className="quienes-container">
+      <div className="overlay">
+      <h1 className="titulo">Quienes somos</h1>
+      <h3 className="subtitulo">UNIDAD EDUCATIVA PARTICULAR<br/>Y DE ALTA CALIDAD</h3>
+     
+      <p className="descripcion">EDUCACION PERSONALIZADA<br/>Y DE ALTA CALIDAD</p>
+      
+      </div>
+    </div>
+  );
+};
