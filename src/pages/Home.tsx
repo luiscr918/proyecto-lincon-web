1f5a33cd19c324e5dc25ec17632e6b5c3cea3bce
DanielMP1995 <danielsantiagomeraparedes@hotmail.com.com>
2025-07-17 10:21:04 -0500

    MisionVision

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,17 @@
+// Home.tsx
+import { useNavigate } from "react-router-dom";
+
 export const Home = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/nosotros"); // Navega a la página /nosotros
+  };
+
   return (
     <div>
       <p>Este es el Home</p>
+      <button onClick={handleClick}>Ir a Nosotros</button>
     </div>
   );
 };
