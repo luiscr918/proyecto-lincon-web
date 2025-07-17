0f5df093df4897c4aea04582defd75b0e5d89abd
Chasi123 <christopherchasiloa56@gmail.com>
2025-07-17 10:31:15 -0500

    img carrusel anim

---
+++ src/components/ImagenesAnimacionHome.tsx
@@ -0,0 +1,69 @@
+const ImagenesAnimacionHome = () => {
+  return (
+    <>
+      <ul className="contenedor">
+        <li>
+          <img src="#" alt="Imagen 1" />
+          <div className="contenido">
+            <span>
+              <h2>jajajaja</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Delectus porro quaerat quas itaque odit exercitationem accusamus
+                necessitatibus cupiditate est, nisi expedita nesciunt dicta
+                minima possimus quis dolor, in corrupti adipisci?
+              </p>
+            </span>
+          </div>
+        </li>
+
+        <li>
+          <img src="#" alt="Imagen 2" />
+          <div className="contenido">
+            <span>
+              <h2>jajajaja</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Delectus porro quaerat quas itaque odit exercitationem accusamus
+                necessitatibus cupiditate est, nisi expedita nesciunt dicta
+                minima possimus quis dolor, in corrupti adipisci?
+              </p>
+            </span>
+          </div>
+        </li>
+
+        <li>
+          <img src="#" alt="Imagen 3" />
+          <div className="contenido">
+            <span>
+              <h2>jajajaja</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Delectus porro quaerat quas itaque odit exercitationem accusamus
+                necessitatibus cupiditate est, nisi expedita nesciunt dicta
+                minima possimus quis dolor, in corrupti adipisci?
+              </p>
+            </span>
+          </div>
+        </li>
+
+        <li>
+          <img src="#" alt="Imagen 4" />
+          <div className="contenido">
+            <span>
+              <h2>jajajaja</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Delectus porro quaerat quas itaque odit exercitationem accusamus
+                necessitatibus cupiditate est, nisi expedita nesciunt dicta
+                minima possimus quis dolor, in corrupti adipisci?
+              </p>
+            </span>
+          </div>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default ImagenesAnimacionHome;
