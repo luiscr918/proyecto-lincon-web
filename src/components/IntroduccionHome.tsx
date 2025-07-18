d040951d6cce0f5016ee3686d0dbf0d5a6820e5f
jeanitaz <jeanzeykoharcode@gmail.com>
2025-07-18 10:25:02 -0500

    introduccion colaborativo

--- src/components/IntroduccionHome.tsx
+++ src/components/IntroduccionHome.tsx
@@ -2,8 +2,27 @@
 const IntroduccionHome = () => {
   return (
     <>
+      <div className="contenedor">
+        <div className="contenido">
+          <div className="introducir">
+            <div className="thumbnail-list">
+              <div className="wreapper">
+
+              </div>
+            </div>
+            <div className="navegacion">
+              <button className="siguiente">
+                <span className="linea"></span>
+                <span className="numero-ordinal"></span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </>
+
   )
 }
 
-export default IntroduccionHome
+export default IntroduccionHome;
---
+++ src/js/introduccion.js
@@ -0,0 +1,18 @@
+var data = [
+    {
+        img: "#",
+        country: "zxlfhsjhf",
+
+    }
+]
+
+const introducir = document.querySelector(".introducir");
+const numeroOrdinal = document.querySelector(".numero-ordinal");
+
+introducir.innerHTML = '';
+numeroOrdinal.innerHTML = '';
+for (let i = 0; i < data.length; i++) {
+    introducir.innerHTML = '';
+
+}
+
