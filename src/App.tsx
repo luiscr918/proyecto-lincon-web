7b7651595f16ff1a4b4deb9f101288e644c92214
luiscr918 <lc9107040@gmail.com>
2025-07-20 12:20:00 -0500

    daniel unir listo

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
-import { Nosotros } from "./pages/Nosotros";
-import { Contactos } from "./pages/Contactos";
+
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/nosotros" element={<Nosotros />} />
-      <Route path="/contactos" element={<Contactos />} />
     </Routes>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,12 @@
 // Home.tsx
-import { useNavigate } from "react-router-dom";
-import { Contactos } from './Contactos';
+
 
 export const Home = () => {
-  const navigate = useNavigate();
 
-  const handleClick = () => {
-    navigate("/nosotros"); 
-    navigate("/contactos"); 
-  };
 
   return (
     <div>
       <p>Este es el Home</p>
-      <button onClick={handleClick}>Ir a Nosotros</button><br />
-      <button onClick={handleClick}>Ir a Contactos</button>
     </div>
   );
 };
