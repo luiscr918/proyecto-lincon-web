f0d99c3dd55b38ae8665ff9c767c56847803dd1e
Chasi123 <christopherchasiloa56@gmail.com>
2025-07-23 10:32:03 -0500

    Card rotativa

--- src/components/HomeComponents/Informacion.tsx
+++ src/components/HomeComponents/Informacion.tsx
@@ -4,86 +4,65 @@ import img2 from "../../assets/imgs/PECN2.png";
 import img3 from "../../assets/imgs/PECN3.png";
 import img4 from "../../assets/imgs/PECN4.png";
 
-
 export const Informacion = () => {
   return (
-    <div>
-      <div className="m-15">
-        <h2 className="text-center mb-4 text-2xl font-extrabold leading-none tracking-tight text-blue-950 md:text-5xl lg:text-6xl">¿Por qué Estudiar Con Nosotros?</h2>
+    <>
+      <div className="m-10">
+        <h2 className="text-center mb-4 text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">
+          ¿Porque Estudiar Con Nosotros?
+        </h2>
       </div>
-      <div className="card-group mt-20">
-      <div>
+      <div className="contene-card">
         <div className="card">
-          <img src={img1} alt="Card image cap" />
-          <div className="layer layer1"></div>
-          <div className="info">
-            <h2><strong>Información</strong></h2>
-            <p>
-              <strong>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Accusantium, necessitatibus! Asperiores amet modi repudiandae,
-              corrupti, illum repellat vero qui vitae assumenda minima aliquid
-              sint, magni dolores provident error nemo. Harum.
-              </strong>
-            </p>
+          <div className="card-inner">
+            <div className="card-front">
+              <img src={img1} alt="" />
+            </div>
+            <div className="card-back">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet corrupti voluptas ex labore neque architecto dolores consequatur 
+                maiores culpa aut. Natus delectus doloribus, 
+                et animi sed corrupti minus neque iste.</p>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-inner">
+            <div className="card-front">
+              <img src={img2} alt="" />
+            </div>
+            <div className="card-back">
+              <p>Lorem ipsum dolor sit amet consectetur 
+                adipisicing elit. Placeat consectetur perspiciatis quidem mollitia 
+                reiciendis officia ullam veniam sapiente, aliquam dolor cumque quibusdam ipsam aliquid laborum, qui adipisci? Nostrum, voluptas officiis.</p>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-inner">
+            <div className="card-front">
+              <img src={img3} alt="" />
+            </div>
+
+            <div className="card-back">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam architecto, eligendi placeat incidunt dolore, consectetur iste veritatis neque iusto illo iure quibusdam eos facere,
+                 similique maiores molestias doloremque nam sed?</p>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-inner">
+            <div className="card-front">
+              <img src={img4} alt="" />
+            </div>
+            <div className="card-back">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Inventore sit blanditiis aperiam labore voluptas veritatis provident sapiente
+                 eligendi quis. Excepturi ipsum libero doloremque voluptas, 
+                animi nulla reprehenderit. Reiciendis, corrupti odit?</p>
+            </div>
           </div>
         </div>
       </div>
-      <div>
-        <div className="card">
-          <img src={img2} alt="Card image cap" />
-          <div className="layer layer2"></div>
-          <div className="info">
-            <h2><strong>Información</strong></h2>
-            <p>
-              <strong>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Accusantium, necessitatibus! Asperiores amet modi repudiandae,
-              corrupti, illum repellat vero qui vitae assumenda minima aliquid
-              sint, magni dolores provident error nemo. Harum.
-              </strong>
-            </p>
-          </div>
-        </div>
-      </div>
-      <div>
-        <div className="card">
-          <img src={img3} alt="Card image cap" />
-          <div className="layer layer3"></div>
-          <div className="info">
-            <h2><strong>Información</strong></h2>
-            <p>
-              <strong>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Accusantium, necessitatibus! Asperiores amet modi repudiandae,
-              corrupti, illum repellat vero qui vitae assumenda minima aliquid
-              sint, magni dolores provident error nemo. Harum.
-              </strong>
-            </p>
-          </div>
-        </div>
-      </div>
-      <div>
-        <div className="card">
-          <img src={img4} alt="Card image cap" />
-          <div className="layer layer4"></div>
-          <div className="info">
-            <h2><strong>Información</strong></h2>
-            <p>
-              <strong>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Accusantium, necessitatibus! Asperiores amet modi repudiandae,
-              corrupti, illum repellat vero qui vitae assumenda minima aliquid
-              sint, magni dolores provident error nemo. Harum.
-              </strong>
-            </p>
-          </div>
-        </div>
-      </div>
-      <script src="../introduccion.js"></script>
-    </div>
-    </div>
-    
+    </>
   );
 };
-
