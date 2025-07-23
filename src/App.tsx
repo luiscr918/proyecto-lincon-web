f3f6de9525c4259ad5bc7a5e45267c9b67d03267
luiscr918 <lc9107040@gmail.com>
2025-07-23 05:58:51 -0500

    login con firebase implementadao

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Home } from "./pages/Home";
 import OfertaAcademica from "./pages/OfertaAcademica";
 import { Contactos } from "./pages/Contactos";
 import { Nosotros } from "./pages/Nosotros";
+import { Login } from "./pages/Login";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/oferta-academica" element={<OfertaAcademica />} />
       <Route path="/contactanos" element={<Contactos />} />
       <Route path="/sobre-nosotros" element={<Nosotros />} />
+      <Route path="/login-encargado" element={<Login />} />
     </Routes>
   );
 }
---
+++ src/pages/Login.tsx
@@ -0,0 +1,10 @@
+import { LoginComponent } from "../components/LoginComponent";
+import Navegacion from "../components/Navegacion";
+export const Login = () => {
+  return (
+    <>
+      <Navegacion />
+      <LoginComponent />
+    </>
+  );
+};
