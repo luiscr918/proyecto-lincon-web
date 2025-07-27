3c199951431c1ef8910109123b583fc66973026a
luiscr918 <lc9107040@gmail.com>
2025-07-27 11:17:50 -0500

    intento 5 subida a web

--- src/const/academicOffer.ts
+++ src/const/academicOffer.ts
@@ -1,9 +1,9 @@
 // src/const/academicOffer.ts
 
-import InicialImage from '../assets/imgs/EducacionInicial.png';
-import BasicaImage from '../assets/imgs/EducacionBasica.png';
-import BachilleratoImage from '../assets/imgs/BachilleratoTecnicoInformatica.png';
-import BasicaSuperiorImage from '../assets/imgs/EducacionBasicaSuperior.png';
+import InicialImage from "../assets/imgs/EducacionInicial.png";
+import BasicaImage from "../assets/imgs/EducacionBasica.png";
+import BachilleratoImage from "../assets/imgs/BachilleratoTecnicoInformatica.png";
+import BasicaSuperiorImage from "../assets/imgs/EducacionBasicaSUperior.png";
 
 interface AcademicLevel {
   id: string;
@@ -19,63 +19,67 @@ interface AcademicLevel {
 
 export const Academia_Oferta_DATA: AcademicLevel[] = [
   {
-    id: 'inicial',
-    title: 'Educación Inicial',
-    ageOrCourseInfo: 'Desde 3 años',
-    description: 'En el centro educativo Lincold Larrea Benalcazar, brindamos una educación inicial sólida y divertida para los más pequeños, sentando las bases de su futuro académico.',
+    id: "inicial",
+    title: "Educación Inicial",
+    ageOrCourseInfo: "Desde 3 años",
+    description:
+      "En el centro educativo Lincold Larrea Benalcazar, brindamos una educación inicial sólida y divertida para los más pequeños, sentando las bases de su futuro académico.",
     features: [
-      'Desarrollo de habilidades motoras y cognitivas.',
-      'Estimulación del lenguaje y la creatividad.',
-      'Fomento de la socialización y autonomía.',
+      "Desarrollo de habilidades motoras y cognitivas.",
+      "Estimulación del lenguaje y la creatividad.",
+      "Fomento de la socialización y autonomía.",
     ],
     imageUrl: InicialImage,
-    altText: 'Niños jugando en educación inicial',
-    cardBgClass: 'bg-white',
-    buttonColorClass: 'text-blue-900 hover:bg-blue-50'
+    altText: "Niños jugando en educación inicial",
+    cardBgClass: "bg-white",
+    buttonColorClass: "text-blue-900 hover:bg-blue-50",
   },
   {
-    id: 'basica',
-    title: 'Educación Básica',
-    ageOrCourseInfo: '1ro a 7mo de EGB',
-    description: 'Formamos estudiantes con bases sólidas en todas las materias, promoviendo el pensamiento crítico, la investigación y la resolución de problemas en un ambiente de aprendizaje activo.',
+    id: "basica",
+    title: "Educación Básica",
+    ageOrCourseInfo: "1ro a 7mo de EGB",
+    description:
+      "Formamos estudiantes con bases sólidas en todas las materias, promoviendo el pensamiento crítico, la investigación y la resolución de problemas en un ambiente de aprendizaje activo.",
     features: [
-      'Currículo actualizado y multidisciplinario.',
-      'Desarrollo de competencias académicas clave.',
-      'Proyectos innovadores e interdisciplinarios.',
+      "Currículo actualizado y multidisciplinario.",
+      "Desarrollo de competencias académicas clave.",
+      "Proyectos innovadores e interdisciplinarios.",
     ],
     imageUrl: BasicaImage,
-    altText: 'Estudiantes en un aula de educación básica',
-    cardBgClass: 'bg-white',
-    buttonColorClass: 'text-blue-900 hover:bg-blue-50'
+    altText: "Estudiantes en un aula de educación básica",
+    cardBgClass: "bg-white",
+    buttonColorClass: "text-blue-900 hover:bg-blue-50",
   },
   {
-    id: 'basica-superior',
-    title: 'Educación Básica Superior',
-    ageOrCourseInfo: '8vo a 10mo de EGB',
-    description: 'En este nivel, fortalecemos las habilidades académicas y personales, preparando a los estudiantes para los desafíos del bachillerato. Enfocamos en el razonamiento lógico, el análisis crítico y la toma de decisiones.',
+    id: "basica-superior",
+    title: "Educación Básica Superior",
+    ageOrCourseInfo: "8vo a 10mo de EGB",
+    description:
+      "En este nivel, fortalecemos las habilidades académicas y personales, preparando a los estudiantes para los desafíos del bachillerato. Enfocamos en el razonamiento lógico, el análisis crítico y la toma de decisiones.",
     features: [
-      'Profundización en áreas de conocimiento.',
-      'Desarrollo de pensamiento lógico y analítico.',
-      'Orientación vocacional temprana.',
+      "Profundización en áreas de conocimiento.",
+      "Desarrollo de pensamiento lógico y analítico.",
+      "Orientación vocacional temprana.",
     ],
     imageUrl: BasicaSuperiorImage,
-    altText: 'Jóvenes estudiantes en educación básica superior',
-    cardBgClass: 'bg-white',
-    buttonColorClass: 'text-blue-900 hover:bg-blue-50'
+    altText: "Jóvenes estudiantes en educación básica superior",
+    cardBgClass: "bg-white",
+    buttonColorClass: "text-blue-900 hover:bg-blue-50",
   },
   {
-    id: 'bachillerato',
-    title: 'Bachillerato Técnico',
-    ageOrCourseInfo: '1ro a 3ro de Bachillerato',
-    description: 'Nuestro Bachillerato Técnico ofrece una formación integral y especializada, preparando a los estudiantes para la educación superior y el mercado laboral. Enfocados en competencias técnicas y profesionales.',
+    id: "bachillerato",
+    title: "Bachillerato Técnico",
+    ageOrCourseInfo: "1ro a 3ro de Bachillerato",
+    description:
+      "Nuestro Bachillerato Técnico ofrece una formación integral y especializada, preparando a los estudiantes para la educación superior y el mercado laboral. Enfocados en competencias técnicas y profesionales.",
     features: [
-      'Programas de estudio especializados.',
-      'Preparación para la universidad y el mundo laboral.',
-      'Desarrollo de habilidades blandas y de investigación.',
+      "Programas de estudio especializados.",
+      "Preparación para la universidad y el mundo laboral.",
+      "Desarrollo de habilidades blandas y de investigación.",
     ],
     imageUrl: BachilleratoImage,
-    altText: 'Jóvenes estudiantes de bachillerato técnico',
-    cardBgClass: 'bg-white',
-    buttonColorClass: 'text-blue-900 hover:bg-blue-50'
+    altText: "Jóvenes estudiantes de bachillerato técnico",
+    cardBgClass: "bg-white",
+    buttonColorClass: "text-blue-900 hover:bg-blue-50",
   },
 ];
