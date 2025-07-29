4113ce47cf5b89c0dab33f6bb32ad7e4e49a589d
luiscr918 <lc9107040@gmail.com>
2025-07-28 22:15:29 -0500

    seccion nosotros quitada imagen

--- src/components/NosotrosComponents/Header.tsx
+++ src/components/NosotrosComponents/Header.tsx
@@ -1,24 +1,23 @@
 export const Header = () => {
   return (
-    <section className="relative w-full h-[85vh] overflow-hidden ">
-      {/* Imagen de fondo con efecto parallax suave */}
-      <div
-        className="absolute inset-0 bg-contain bg-center bg-no-repeat mt-1"
-        style={{
-          backgroundImage: `url(https://i.pinimg.com/originals/59/34/6b/59346b4b5c80cc5cf0c483a27dfdcb36.gif)`,
-        }}
-      ></div>
-
+    <section className="relative w-full h-auto overflow-hidden ">
       {/* Contenido centrado */}
       <div className="relative z-10 flex flex-col items-center justify-center h-full text-white text-center px-4 ">
-        <h1 className="text-4xl md:text-5xl font-bold mb-4">¿Quiénes somos?</h1>
-        <h3 className="text-xl md:text-2xl font-semibold mb-2">
-          UNIDAD EDUCATIVA PARTICULAR <br />
-          <span className="italic">"LINCOLN LARREA BENALCÁZAR"</span>
-        </h3>
-        <p className="text-lg md:text-xl">
+        <h1 className="mb-4 text-6xlxl font-extrabold leading-none tracking-tight  md:text-5xl lg:text-6xl ">
+          ¿Quiénes somos?
+        </h1>
+        <br />
+        <h2 className="text-5xl font-semibold">
+          UNIDAD EDUCATIVA PARTICULAR
+          <br />
+          <span className="text-transparent text-4xl bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+            "LINCOLN LARREA BENALCÁZAR"
+          </span>{" "}
+        </h2>
+        <br />
+        <h2 className="text-3xl font-normal">
           EDUCACIÓN PERSONALIZADA <br />Y DE ALTA CALIDAD
-        </p>
+        </h2>
       </div>
     </section>
   );
